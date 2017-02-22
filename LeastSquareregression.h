22e5148839859f58b704ad34288d7452fca21efe
Jan Trochta <j.trochta@gmail.com>
2017-02-22 10:50:05 +0100

    Update LeastSquareregression.h

--- LeastSquareregression.h
+++ LeastSquareregression.h
@@ -16,7 +16,7 @@
 #ifndef LEASTSQUAREREGRESSION_H_INCLUDED
 #define LEASTSQUAREREGRESSION_H_INCLUDED
 
-#include "Cloud.h"
+#include "cloud.h"
 #include <pcl/pcl_base.h>
 #include <pcl/point_types.h>
 #include <pcl/common/common_headers.h>
